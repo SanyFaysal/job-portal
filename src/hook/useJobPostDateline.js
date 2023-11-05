ba0bb6ba8d25ed5745dca5bb4959394314b1db6c
Abu Sani Faysal <97028772+SanyFaysal@users.noreply.github.com>
2023-11-05 15:15:50 +0600

    dateline modified

--- src/hook/useJobPostDateline.js
+++ src/hook/useJobPostDateline.js
@@ -1,22 +1,38 @@
-import moment from "moment"
+import moment from 'moment';
 
 export const useJobPostDateLine = (job) => {
-    const currentDateFormat = moment.utc(Date.now()).format('MM/DD/YYYY');
-    const dateLineFormat = moment.utc(job?.dateline).format('MM/DD/YYYY')
-    const date1 = new Date(currentDateFormat);
-    const date2 = new Date(dateLineFormat);
-    const diffTime = Math.abs(date2 - date1);
-    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    const currentDateFormat = moment.utc(); // Get current date in UTC
+    const dateLineFormat = moment.utc(job?.dateline, 'MM/DD/YYYY'); // Parse job's dateline in UTC
+
+    const diffDays = dateLineFormat.diff(currentDateFormat, 'days'); // Calculate difference in days
+
     if (diffDays > 1) {
-
-        return { status: true, msg: `${diffDays} days remaining to apply`, color: 'text-green-500 bg-green-50' };
+        return {
+            status: true,
+            msg: `${diffDays} days remaining to apply`,
+            color: 'text-green-500 bg-green-50'
+        };
     }
+
+    if (diffDays === 1) {
+        return {
+            status: true,
+            msg: 'Tomorrow is remaining to apply',
+            color: 'bg-yellow-100 text-yellow-500'
+        };
+    }
+
     if (diffDays === 0) {
-
-        return { status: true, msg: 'Today is remaining to apply', color: 'bg-red-100 text-red-500' };
+        return {
+            status: true,
+            msg: 'Today is the last day to apply',
+            color: 'bg-red-100 text-red-500'
+        };
     }
 
-    return { status: false, msg: 'Dateline in over !', color: 'bg-[#f9eaea] text-red-500 font-bold' };
-
-
-}
+    return {
+        status: false,
+        msg: 'Dateline is over!',
+        color: 'bg-[#f9eaea] text-red-500 font-bold'
+    };
+};
